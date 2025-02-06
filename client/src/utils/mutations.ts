2d7389fdc40d1eb460428e8672ff5f610a53bf07
Sierra <sierraolson217@gmail.com>
2025-02-05 19:19:41 -0600

    fixed missing bracket

--- client/src/utils/mutations.ts
+++ client/src/utils/mutations.ts
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client';
 
 export const ADD_USER = gql`
     mutation AddUser($username: String!, $email: String!, $password: String!) {
-        addUser(username: $username, email:$email, password: $password)
+        addUser(username: $username, email:$email, password: $password) {
             token
             user {
                 username
@@ -13,7 +13,7 @@ export const ADD_USER = gql`
 `;
 export const LOGIN_USER = gql`
     mutation loginUser($email: String!, $password: String!) {
-        login(email: $email, password: $password)
+        login(email: $email, password: $password) {
             token
             user {
                 username
